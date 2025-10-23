2b8508840316eb10c1274bba382edfcdc5c47903
Joel Alftberg <joel.alftberg98@gmail.com>
2025-10-23 20:39:27 +0200

    Adds rounding and formatting data for P2

--- L12/Till elektroniken bitmanipulation/yrgo/main.c
+++ L12/Till elektroniken bitmanipulation/yrgo/main.c
@@ -9,34 +9,157 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+// display_buffer[0] = integer, display_buffer[1] = decimal, display_buffer[2] = decimal_point;
+uint16_t display_buffer[3] = {};
+        
 int main(void)
 {
-    setup_p1();
+    setup_p0();
     
-        /*
-    Programmet läser av POT1 och delar upp det i 8 delar och visar sedan resultatet via binärkod
-    på LED 1,2 och 3 enligt tabellen nedan
-    
-    Värde   Binärt  POT-Värde
-    -------------------------
-    0       000       0 - 127
-    1       001     128 - 255
-    2       010     256 - 383
-    3       011     384 - 511
-    4       100     512 - 639
-    5       101     640 - 767
-    6       110     768 - 895
-    7       111     896 - 1023
-    */
-        uint8_t pot_value = 0;
-    while(1) {
-        /*Läser av och skalar ner pot-värdet så vi får ett värde mellan 0 - 7*/
-        pot_value = read_pot1()/128;
+   /* 
+   if d7 high -> Rset = 330 else if d8 -> high Rset = 33000
+   
+   Read from A0 - 16 samples with for loop (hope for some noise)
+   average = divide A0 by 16
+   voltage_read = average value * (5.0f / 1023.0f)
+   R_measured = (voltage_read * R_set) / (5 - voltage_read);
+   
+   */
+               
+    while(1){
         
-        /*Här används bitmanipulation för att "plocka ut" de tre olika bitarna som skall
-        tända/släcka aktuell led för att visa det binära talet*/
-        led(3,(pot_value & 0b00000001));
-        led(2,(pot_value & 0b00000010));
-        led(1,(pot_value & 0b00000100));
     }
+        
+        
+        // if power just turned on, do some kind of starting sequence (init)
+        // check the mode we are in
+            // if mode has changed since last loop, display mode (ex U1 for low voltage, U2 for high voltage)
+        
+        // depending on mode, do appropriate math
+        // handle number output by rounding and formatting
+        // display number
+
+}
+
+/*
+
+Takes a 3 digit decimal part number as input and then rounds it up to the nearest hundreth which for
+a 3 digit number is it's nearest ten. It then returns two digit to represent the hundreds part.
+Example, number = 789 (0.789) round to nearest hundreth so  789 -> 790 (0.790) then /= 10 so it becomes 79
+
+*/
+uint16_t round_to_hundreth(uint16_t decimal_part){
+    // number = 789 (0.789) round to nearest tenth so  789 -> 790 (0.790) then /= 10 so it becomes 79
+    uint8_t remainder = decimal_part % 10;
+    if (remainder > 5){
+        decimal_part += (10 - remainder);
+    }
+    else if (remainder < 5 && remainder != 0){
+        decimal_part -= remainder;
+    }
+    
+    return (decimal_part / 10);
+}
+
+/*
+
+Takes a 3 digit decimal part number as input and then rounds it up to the nearest tenth which for
+a 3 digit number is it's nearest hundred. It then returns a single digit to represent the tenth part.
+Example, number = 789 (0.789) round to nearest tenth so  789 -> 800 (0.800) then /= 100 so it becomes 8
+
+*/
+uint16_t round_to_tenth(uint16_t decimal_part){
+
+    uint8_t remainder = decimal_part % 100;
+    if (remainder > 50){
+        decimal_part += (100 - remainder);
+    }
+    else if (remainder < 50 && remainder != 0){
+        decimal_part -= remainder;
+    }
+    
+    return (decimal_part / 100);
+}
+
+/*
+
+Takes a positive float as an input (value between 0 - 100000) and updates the display_buffer[]
+array with the value rounded and converted to a suitable format for a 3-digit display
+
+*/
+void display_buffer_update(float number){
+    
+    uint16_t num_decimal_part = 0;
+
+    if (number < 0){
+        // {404, 404, 3} in display buffer will evaluate to ERR
+        display_buffer[0] = 404;
+        display_buffer[1] = 404;
+        display_buffer[2] = 3;
+        return;
+    }
+    // 1k to 99.9k range
+    if (number > 1000 && number < 100000){
+
+        uint8_t num_thousands = number /= 1000;
+        num_decimal_part = number - (num_thousands * 1000);
+        display_buffer[0] = num_thousands;
+        
+        if (num_thousands < 10){
+            // example 7825 -> num_decimal = 825 (0.825), round to nearest hundreth 830 (0.830) and then do /= 10 so it becomes 83
+            
+            display_buffer[1] = round_to_hundreth(num_decimal_part);
+            display_buffer[2] = 1;         
+        }
+        else if (num_thousands > 10){
+            // example 24789 -> num_decimal = 789 (0.789) round to nearest tenth so  789 -> 800 (0.800) then /= 100 so it becomes 8
+            display_buffer[1] = round_to_tenth(num_decimal_part);
+            display_buffer[2] = 2;
+        }
+    }
+    // 0 to 999 range 
+    else if (number > 0 && number < 1000){
+        
+        uint16_t num_integer_part = (uint16_t) number;
+        num_decimal_part = (number - num_integer_part) * 1000;
+
+        // example 6.789123   
+        if (number < 10 && number > 0){
+            display_buffer[1] = round_to_hundreth(num_decimal_part);
+            display_buffer[2] = 1;  
+        }
+        // example 48.14722
+        else if (number > 10 && number < 100){
+            display_buffer[1] = round_to_tenth(num_decimal_part);
+            display_buffer[2] = 1;
+        }
+        // example 453.51452
+        else if (number > 10 && number < 1000){
+            
+            if (num_decimal_part > 500){
+                num_integer_part++;
+            }
+            
+            display_buffer[1] = 0;
+            display_buffer[2] = 3;
+            
+        }
+        
+        display_buffer[0] = num_integer_part;
+        
+    }              
+}
+
+/*
+
+Send the data present in the buffer to the 3 digit 7-segment display
+Use the HAL functions for outputting digits found in the drivers.c file
+
+TODO: write drivers first for sending a number to a specific digit ex.
+display_digit(uint8_t number, uint8_t digit, uint8_t decimal_point);
+display_codes(uint8_t mode);
+
+*/
+void display_buffer_send(void){
+    
 }
---
+++ L12/Till elektroniken bitmanipulation/yrgo/math.c
@@ -0,0 +1,6 @@
+/*
+ * math_helpers.c
+ *
+ * Created: 2025-10-23 14:11:34
+ *  Author: joela
+ */ 
